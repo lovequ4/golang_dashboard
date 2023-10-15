973f84e77387f28b57b656bccad1436906acb2ce
sam <a0983131803@gmail.com>
2023-10-15 20:21:49 +0800

    user model

---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import "time"
+
+type User struct {
+	Id       int       `gorm:"primaryKey;AUTO_INCREMENT" json:"id"`
+	Name     string    `gorm:"type:varchar(25);not null" json:"name"`
+	Password string    `gorm:"type:varchar(255);not null" json:"password"`
+	Email    string    `gorm:"type:varchar(50);unique;not null" json:"email"`
+	Identity string    `gorm:"type:varchar(10);not null;default:'employee'" json:"identity"`
+	Date     time.Time `gorm:"type:datetime;not null" json:"date"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
