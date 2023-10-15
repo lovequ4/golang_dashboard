bfe16c6654e0da8da11eb32c038684d1c80413a1
sam <a0983131803@gmail.com>
2023-10-15 20:20:28 +0800

    add user router

--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"Demo/database"
+	_ "Demo/docs"
+	"Demo/router"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func init() { //main() 函數之前被調用
 	database.DBconnect()
 }
 
+// @title  MyGolang
+// @version 1.0
+// @description  Swagger API.
+// @host localhost:8080
 func main() {
 	r := gin.Default()
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.UserRoutes(r) // 引入user router
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(
+		swaggerFiles.Handler,
+		ginSwagger.URL("http://localhost:8080/swagger/doc.json"),
+	))
+
+	r.Run(":8080")
 }
