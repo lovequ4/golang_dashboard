40a6204cbfdec9d95cda3122c0cc015db5b96e3f
sam <a0983131803@gmail.com>
2023-10-16 20:00:19 +0800

    GenerateToken add name & role

--- middlewares/token.go
+++ middlewares/token.go
@@ -1,6 +1,7 @@
-package token
+package middlewares
 
 import (
+	"errors"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -8,12 +9,29 @@ import (
 
 var jwtSecret = []byte("secret")
 
-func GenerateToken(userId int) (string, error) {
+func GenerateToken(userId int, name string, role string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"userId": userId,
+		"name":   name,
+		"role":   role,
 		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
 
 	tokenString, _ := token.SignedString(jwtSecret)
-	return tokenString, nil
+	return "Bearer " + tokenString, nil
+}
+
+//解析JWT
+func ParseToken(tokenString string) (*jwt.MapClaims, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (i interface{}, err error) {
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
 }
