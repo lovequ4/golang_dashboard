3d76dc76cd5fe980487b3dc33240beb0d7279178
sam <a0983131803@gmail.com>
2023-10-16 19:59:25 +0800

    change identity to role

--- models/user.go
+++ models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	Name     string    `gorm:"type:varchar(25);not null" json:"name"`
 	Password string    `gorm:"type:varchar(255);not null" json:"password"`
 	Email    string    `gorm:"type:varchar(50);unique;not null" json:"email"`
-	Identity string    `gorm:"type:varchar(10);not null;default:'employee'" json:"identity"`
+	Role     string    `gorm:"type:varchar(10);not null;default:'employee'" json:"role"`
 	Date     time.Time `gorm:"type:datetime;not null" json:"date"`
 }
 
